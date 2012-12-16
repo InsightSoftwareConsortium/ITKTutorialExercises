aa0b5d40c375349718c2bcf76316c8f1d96e9e53
Luis Ibanez <luis.ibanez@kitware.com>
2012-12-16 18:25:11 -0500

    GetRotationMatrix() method deprecated.
    Replaced with simply GetMatrix().

--- Source/exercise22/registration.cxx
+++ Source/exercise22/registration.cxx
@@ -195,7 +195,7 @@ int main( int argc, char *argv[] )
 
   transform->SetParameters( finalParameters );
 
-  TransformType::MatrixType matrix = transform->GetRotationMatrix();
+  TransformType::MatrixType matrix = transform->GetMatrix();
   TransformType::OffsetType offset = transform->GetOffset();
 
   std::cout << "Matrix = " << std::endl << matrix << std::endl;
